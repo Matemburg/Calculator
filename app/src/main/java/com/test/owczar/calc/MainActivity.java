8695f88857c6be988990fba214224ddb5f90d5f9
Mateusz <38332261+Matemburg@users.noreply.github.com>
2018-12-12 15:18:02 +0100

    0.1
    Smple App with two Activites

---
+++ app/src/main/java/com/test/owczar/calc/MainActivity.java
@@ -0,0 +1,29 @@
+package com.test.owczar.calc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import org.mariuszgromada.math.mxparser.*;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText =  findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+
+
+}
