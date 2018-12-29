5c3fc833bf1a528dacb1949f7a8ffa2fb332ea43
Mateusz <38332261+Matemburg@users.noreply.github.com>
2018-12-29 14:33:35 +0100

    SQL Added

--- app/src/main/java/com/test/owczar/calc/DisplayMessageActivity.java
+++ app/src/main/java/com/test/owczar/calc/DisplayMessageActivity.java
@@ -1,12 +1,20 @@
 package com.test.owczar.calc;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    TextView history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +24,25 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        ArrayList<String> History = getIntent().getStringArrayListExtra("History");
+        myDb = new DatabaseHelper(this);
+        Cursor res = myDb.getAllData();
+        if (res.getCount() == 0) {
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            buffer.append("ID: " + res.getString(0) + "\n");
+            buffer.append("Expression: " + res.getString(1) + "\n");
+        }
+        history = findViewById(R.id.history);
+        history.setMovementMethod(new ScrollingMovementMethod());
+        history.append(buffer.toString());
+    }
 
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
-        String message = "";
-        for (int i=0;i<History.size();i++)
-            message=message+History.get(i)+"\n";
-        textView.setText(message);
+    public void clearHistory(View view) {
+        myDb.deleteData();
+        Toast.makeText(this, "History cleared", Toast.LENGTH_LONG).show();
+        history.setText("Empty");
 
     }
 }
--- app/src/main/java/com/test/owczar/calc/MainActivity.java
+++ app/src/main/java/com/test/owczar/calc/MainActivity.java
@@ -1,36 +1,44 @@
 package com.test.owczar.calc;
 
-import android.animation.TypeConverter;
-import android.app.ActionBar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import org.mariuszgromada.math.mxparser.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     TextView Wynik;
-    EditText editText;private List<String> History;
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    EditText editText;
+    //private List<String> History;
+
+    DatabaseHelper myDb;
+
+    //public static final String EXTRA_MESSAGE = "Kalkulator";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        History = new ArrayList<String>();
+      //  History = new ArrayList<String>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Wynik = findViewById(R.id.textView2);
         editText =  findViewById(R.id.editText);
+        myDb = new DatabaseHelper(this);
     }
 
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        intent.putStringArrayListExtra("History", (ArrayList<String>) History);
+       // intent.putStringArrayListExtra("History", (ArrayList<String>) History);
         startActivity(intent);
     }
 
@@ -40,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
 
         Expression math = new Expression(editText.getText().toString());
         Wynik.setText(String.valueOf(math.calculate()));
-        History.add(editText.getText().toString() + " = " + String.valueOf(math.calculate()) );
+       // History.add(editText.getText().toString() + " = " + String.valueOf(math.calculate()) );
+
+        if(myDb.insertData(editText.getText().toString()))
+            Toast.makeText(MainActivity.this, "Data inserted", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
     }
 }
